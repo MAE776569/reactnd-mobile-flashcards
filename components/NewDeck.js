b84bfd8e9cd315eed8873cb8db95995966dcd345
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-09 15:17:22 +0200

    Added form to new deck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,10 +1,60 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React, { Component } from "react"
+import { KeyboardAvoidingView, StyleSheet } from "react-native"
+import { Headline, TextInput, Button } from "react-native-paper"
 
-export default function NewDeck(){
-  return (
-    <View>
-      <Text>Create new deck</Text>
-    </View>
-  )
+class NewDeck extends Component {
+  state = {
+    deckTitle: "",
+    loading: false
+  }
+
+  submitDeck = () => {
+    this.setState({
+      deckTitle: "",
+      loading: true
+    })
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <Headline style={{ marginBottom: 12, textAlign: "center" }}>
+          What is the title of your new deck?
+        </Headline>
+        <TextInput
+          style={{ marginBottom: 12 }}
+          mode="outlined"
+          label="Deck title"
+          value={this.state.deckTitle}
+          onChangeText={deckTitle => this.setState({ deckTitle })}
+        />
+        <Button
+          style={styles.submitBtn}
+          mode="contained"
+          loading={this.state.loading}
+          onPress={this.submitDeck}>
+          Submit
+        </Button>
+      </KeyboardAvoidingView>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 12
+  },
+  submitBtn: {
+    alignSelf: "center",
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 5,
+    paddingBottom: 5,
+    fontSize: 20,
+    width: "50%"
+  }
+})
+
+export default NewDeck
