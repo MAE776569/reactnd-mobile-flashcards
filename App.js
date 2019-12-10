99f0f5dcfc2a50cb4283dcf60bda6ade3b65f16b
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-10 13:40:54 +0200

    Added store to the app

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { DefaultTheme, Provider as PaperProvider } from "react-native-paper"
 import Constants from "expo-constants"
 import { primary, accent } from "./utils/colors"
 import TabNavigator from "./components/TabNavigator"
+import { Provider as StoreProvider } from "react-redux"
+import store from "./store"
 
 const theme = {
   ...DefaultTheme,
@@ -16,21 +18,23 @@ const theme = {
 
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
-      <View style={{ flex: 1 }}>
-        <View
-          style={{
-            backgroundColor: primary,
-            height: Constants.statusBarHeight
-          }} >
-          <StatusBar
-            translucent
-            backgroundColor={primary}
-            barStyle="light-content"
-          />
+    <StoreProvider store={store}>
+      <PaperProvider theme={theme}>
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              backgroundColor: primary,
+              height: Constants.statusBarHeight
+            }}>
+            <StatusBar
+              translucent
+              backgroundColor={primary}
+              barStyle="light-content"
+            />
+          </View>
+          <TabNavigator />
         </View>
-        <TabNavigator />
-      </View>
-    </PaperProvider>
+      </PaperProvider>
+    </StoreProvider>
   )
 }
