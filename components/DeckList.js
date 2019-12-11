c6acc560ed819968f47a90e59d54d9c2489fbe2b
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-11 12:07:35 +0200

    Get decks from state

--- components/DeckList.js
+++ components/DeckList.js
@@ -6,32 +6,16 @@ import DeckItem from "./DeckItem"
 
 class DeckList extends Component {
   render() {
-    const DATA = [
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-        cards: 3
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-        cards: 2
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-        cards: 1
-      }
-    ]
+    const { decks } = this.props
 
     return (
       <FlatList
-        contentContainerStyle={!DATA.length && styles.container}
+        contentContainerStyle={!decks.length && styles.container}
         ItemSeparatorComponent={Divider}
         ListEmptyComponent={<Text>No Decks!</Text>}
-        data={DATA}
+        data={decks}
         renderItem={({ item }) => (
-          <DeckItem title={item.title} description={`${item.cards} cards`} />
+          <DeckItem title={item.title} description={`${item.questions.length} cards`} />
         )}
         keyExtractor={item => item.id}
       />
@@ -47,4 +31,14 @@ const styles = StyleSheet.create({
   }
 })
 
-export default connect()(DeckList)
+function mapStateToProps({ decks }){
+  const deckList = Object.keys(decks).map((deck) => ({
+    id: deck,
+    ...decks[deck]
+  }))
+  return {
+    decks: deckList
+  }
+}
+
+export default connect(mapStateToProps)(DeckList)
