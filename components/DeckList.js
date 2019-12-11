6602f9eb5edbed872058e470811e20aebf453d33
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-11 12:56:09 +0200

    Receive decks from store

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react"
-import { FlatList, StyleSheet, Text } from "react-native"
-import { Divider } from "react-native-paper"
+import { FlatList, StyleSheet, Text, View } from "react-native"
+import { Divider, ActivityIndicator } from "react-native-paper"
 import { connect } from "react-redux"
 import DeckItem from "./DeckItem"
+import { fetchDecks } from "../utils/helpers"
+import { receiveDecks } from "../actions/decks"
 
 class DeckList extends Component {
+  state = {
+    loading: true
+  }
+
+  componentDidMount() {
+    const { getDecks } = this.props
+    getDecks().then(() => {
+      this.setState({ loading: false })
+      console.log(this.props.decks)
+    })
+  }
+
   render() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
+
     const { decks } = this.props
 
     return (
@@ -15,7 +37,10 @@ class DeckList extends Component {
         ListEmptyComponent={<Text>No Decks!</Text>}
         data={decks}
         renderItem={({ item }) => (
-          <DeckItem title={item.title} description={`${item.questions.length} cards`} />
+          <DeckItem
+            title={item.title}
+            description={`${item.questions.length} cards`}
+          />
         )}
         keyExtractor={item => item.id}
       />
@@ -31,8 +56,8 @@ const styles = StyleSheet.create({
   }
 })
 
-function mapStateToProps({ decks }){
-  const deckList = Object.keys(decks).map((deck) => ({
+function mapStateToProps({ decks }) {
+  const deckList = Object.keys(decks).map(deck => ({
     id: deck,
     ...decks[deck]
   }))
@@ -41,4 +66,10 @@ function mapStateToProps({ decks }){
   }
 }
 
-export default connect(mapStateToProps)(DeckList)
+function mapDispatchToProps(dispatch) {
+  return {
+    getDecks: () => fetchDecks().then(decks => dispatch(receiveDecks(decks)))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DeckList)
