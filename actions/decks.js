f214da47199d0076ceab8795f9f751cbaa6777fd
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-11 13:42:43 +0200

    Handle receive decks redux thunk

--- actions/decks.js
+++ actions/decks.js
@@ -1,14 +1,25 @@
+import { fetchDecks } from "../utils/helpers"
+
 export const RECEIVE_DECKS = "RECEIVE_DECKS"
 export const ADD_DECK = "ADD_DECK"
 
-export function receiveDecks(decks){
+function receiveDecks(decks) {
   return {
     type: RECEIVE_DECKS,
     decks
   }
 }
 
-export function addDeck(deck){
+export function handleReceiveDecks(cb) {
+  return dispatch => {
+    return fetchDecks()
+      .then(decks => dispatch(receiveDecks(decks)))
+      .then(cb)
+      .catch(error => console.log("Error fetching decks", error))
+  }
+}
+
+export function addDeck(deck) {
   return {
     type: ADD_DECK,
     deck
