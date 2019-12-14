4cb94e0965eeaffc18700dc0abc290e16c120b23
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-14 19:25:02 +0200

    Created function to get deck's key

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,17 @@
 import { AsyncStorage } from "react-native"
 
+const DECKS_KEY = "DECKS"
+
+export function getDeckKey(title) {
+  return title
+    .trim()
+    .replace(/[^\w\s]/gi, "")
+    .replace(/\s+/, " ")
+    .split(" ")
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join("")
+}
+
 export function fetchDecks() {
-  return AsyncStorage.getItem("deck")
-    .then(JSON.parse)
+  return AsyncStorage.getItem(DECKS_KEY).then(JSON.parse)
 }
