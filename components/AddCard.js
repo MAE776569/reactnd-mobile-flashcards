63eeffce31e096842adb0096396cc10baef9f0d2
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-19 13:46:11 +0200

    Change state when input

--- components/AddCard.js
+++ components/AddCard.js
@@ -3,7 +3,15 @@ import { KeyboardAvoidingView, StyleSheet } from "react-native"
 import { TextInput, Button } from "react-native-paper"
 
 class AddCard extends Component {
+  state = {
+    question: "",
+    answer: "",
+    loading: false
+  }
+
   render() {
+    const { question, answer, loading } = this.state
+
     return (
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
         <TextInput
@@ -12,6 +20,9 @@ class AddCard extends Component {
           placeholder="What is a component?"
           style={styles.mb20}
           autoFocus
+          ref={input => (this.questionInput = input)}
+          value={question}
+          onChangeText={question => this.setState({ question })}
         />
         <TextInput
           mode="outlined"
@@ -19,8 +30,13 @@ class AddCard extends Component {
           placeholder="Component let you split the UI into independent, reusable pieces"
           multiline={true}
           style={styles.mb20}
+          ref={input => (this.answerInput = input)}
+          value={answer}
+          onChangeText={answer => {
+            this.setState({ answer })
+          }}
         />
-        <Button mode="contained" style={styles.submitBtn}>
+        <Button mode="contained" style={styles.submitBtn} loading={loading}>
           Submit
         </Button>
       </KeyboardAvoidingView>
