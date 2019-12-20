07f641c46a6f47be014c6f9b82b414c440835d97
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-20 16:33:27 +0200

    Handle save card action

--- actions/cards.js
+++ actions/cards.js
@@ -1,9 +1,19 @@
+import { saveCard } from "../utils/helpers"
+
 export const ADD_CARD = "ADD_CARD"
 
-function addCard(deckId, card){
+function addCard(deckId, card) {
   return {
     type: ADD_CARD,
     deckId,
     card
+  }
+}
+
+export function handleAddCard(deckId, card) {
+  return dispatch => {
+    return saveCard(deckId, card)
+      .then(dispatch(addCard(deckId, card)))
+      .catch(error => console.log("Error saving card", error))
   }
 }
