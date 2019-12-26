a1252e091be57f5238706be4b90103cffc2dd2bf
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-26 13:59:06 +0200

    Created notification helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,6 @@
 import { AsyncStorage } from "react-native"
+import { Notifications } from "expo"
+import * as Permissions from "expo-permissions"
 
 export function DeckTitleIsValid(title) {
   return (
@@ -20,15 +22,17 @@ export function getDeckKey(title) {
 }
 
 export function fetchDecks() {
-  return AsyncStorage.getAllKeys().then(keys =>
-    AsyncStorage.multiGet(keys).then(pairs =>
-      pairs.length
-        ? pairs.map(([key, value]) =>
-            Object.assign(JSON.parse(value), { id: key })
-          )
-        : pairs
+  return AsyncStorage.getAllKeys()
+    .then(keys => keys.filter(key => key !== NOTIFICATION_KEY))
+    .then(keys =>
+      AsyncStorage.multiGet(keys).then(pairs =>
+        pairs.length
+          ? pairs.map(([key, value]) =>
+              Object.assign(JSON.parse(value), { id: key })
+            )
+          : pairs
+      )
     )
-  )
 }
 
 export function storeDeck(deck) {
@@ -67,4 +71,48 @@ export function answerIsCorrect(questionAnswer, userChoice) {
     (correctOptions.includes(cleanAnswer) && userChoice) ||
     (incorrectOptions.includes(cleanAnswer) && !userChoice)
   )
+}
+
+const NOTIFICATION_KEY = "DAILY_REMINDER"
+
+function createNotification() {
+  return {
+    title: "Start a new quiz!",
+    body: "Don't forget to finish your quizzes for today!",
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: "high",
+      sticky: false,
+      vibrate: true
+    }
+  }
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then(data => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
+          if (status === "granted") {
+            Notifications.cancelAllScheduledNotificationsAsync()
+
+            let tomorrow = new Date()
+            tomorrow.setDate(tomorrow + 1)
+            tomorrow.setHours(20)
+            tomorrow.setMinutes(0)
+
+            Notifications.scheduleLocalNotificationAsync(createNotification(), {
+              time: tomorrow,
+              repeat: "day"
+            })
+
+            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+          }
+        })
+      }
+    })
 }
