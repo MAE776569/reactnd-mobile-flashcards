332ccb054fe5cdb751937b3fdf26229afd200d30
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-12-26 14:44:55 +0200

    Handle IOS tab navigation

--- components/TabNavigator.js
+++ components/TabNavigator.js
@@ -1,45 +1,53 @@
 import React from "react"
 import DeckList from "./DeckList"
 import NewDeck from "./NewDeck"
-import { createMaterialTopTabNavigator } from "react-navigation-tabs"
+import {
+  createMaterialTopTabNavigator,
+  createBottomTabNavigator
+} from "react-navigation-tabs"
 import { primary } from "../utils/colors"
 import { MaterialCommunityIcons, FontAwesome } from "@expo/vector-icons"
+import { Platform } from "react-native"
 
-const TabNavigator = createMaterialTopTabNavigator(
-  {
-    Decks: {
-      screen: DeckList,
-      navigationOptions: {
-        tabBarLabel: "Decks",
-        tabBarIcon: ({ tintColor }) => (
-          <MaterialCommunityIcons name="cards" color={tintColor} size={25} />
-        )
-      }
-    },
-    NewDeck: {
-      screen: NewDeck,
-      navigationOptions: {
-        tabBarLabel: "New Deck",
-        tabBarIcon: ({ tintColor }) => (
-          <FontAwesome name="plus-square" color={tintColor} size={25} />
-        )
-      }
+const navigationScreens = {
+  Decks: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: "Decks",
+      tabBarIcon: ({ tintColor }) => (
+        <MaterialCommunityIcons name="cards" color={tintColor} size={25} />
+      )
     }
   },
-  {
-    tabBarOptions: {
-      style: {
-        backgroundColor: primary
-      },
-      showIcon: true,
-      tabStyle: {
-        flexDirection: "row"
-      }
-    },
+  NewDeck: {
+    screen: NewDeck,
     navigationOptions: {
-      header: null
+      tabBarLabel: "New Deck",
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name="plus-square" color={tintColor} size={25} />
+      )
     }
   }
-)
+}
+
+const navigationOptions = {
+  tabBarOptions: {
+    style: {
+      backgroundColor: primary
+    },
+    showIcon: true,
+    tabStyle: {
+      flexDirection: Platform.OS === "ios" ? "column" : "row"
+    }
+  },
+  navigationOptions: {
+    header: null
+  }
+}
+
+const TabNavigator =
+  Platform.OS === "ios"
+    ? createBottomTabNavigator(navigationScreens, navigationOptions)
+    : createMaterialTopTabNavigator(navigationScreens, navigationOptions)
 
 export default TabNavigator
